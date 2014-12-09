295a0f7f8f9dc2a8f3fc616b6c557d06552062cf
zlwen <450275803@qq.com>
2014-12-09 22:33:53 +0800

    change to annotation

--- src/cn/edu/pku/ss/crypto/abe/Policy.java
+++ src/cn/edu/pku/ss/crypto/abe/Policy.java
@@ -5,15 +5,21 @@ import it.unisa.dia.gas.jpbc.Element;
 import java.util.ArrayList;
 import java.util.List;
 
+import cn.edu.pku.ss.crypto.abe.serialize.Serializable;
 import cn.edu.pku.ss.crypto.abe.serialize.SimpleSerializable;
 
 public class Policy implements SimpleSerializable{
 	/* serialized */
+	@Serializable
 	int k;            /* one if leaf, otherwise threshold */
+	@Serializable
 	String attr;       /* attribute string if leaf, otherwise null */
+	@Serializable(group="G1")
 	Element Cy;      /* G_1, only for leaves */
+	@Serializable(group="G1")
 	Element _Cy;     /* G_1, only for leaves */
 //	List<Policy> children; /* pointers to bswabe_policy_t's, len == 0 for leaves */
+	@Serializable
 	Policy[] children;
 	
 	/* only used during encryption */
--- src/cn/edu/pku/ss/crypto/abe/PublicKey.java
+++ src/cn/edu/pku/ss/crypto/abe/PublicKey.java
@@ -2,21 +2,20 @@ package cn.edu.pku.ss.crypto.abe;
 
 import it.unisa.dia.gas.jpbc.Element;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.lang.reflect.Field;
-
+import cn.edu.pku.ss.crypto.abe.serialize.Serializable;
 import cn.edu.pku.ss.crypto.abe.serialize.SimpleSerializable;
 
 public class PublicKey implements SimpleSerializable {
+	@Serializable(group="G1")
 	Element g; // G1 generator
+	
+	@Serializable(group="G2")
 	Element gp; // G2 generator
+	
+	@Serializable(group="GT")
 	Element g_hat_alpha; // GT
+	
+	@Serializable(group="G1")
 	Element h; //G1
 
 }
